e6ceef39acb23e6f77646e7be139ad647e3d9ddf
Hasan <hasanozdemir1@trakya.edu.tr>
2021-11-23 23:36:34 +0300

    Basic frontend designed

---
+++ static/js/upload/upload.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $(".slider-1, .slider-2").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        prevArrow: false,
+        nextArrow: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    })
+
+});
+    
