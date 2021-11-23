a3de45634ea97e40568f412685e29a4c43cbeb37
Hasan <hasanozdemir1@trakya.edu.tr>
2021-11-23 16:59:15 +0300

    routings are updated. random_select and upload_file root added

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+# http://127.0.0.1.5000
+@app.route('/')
+def welcome_page():  # put application's code here
+    return render_template('welcome/welcome_page.html')
+
+@app.route('/random_select')
+def random_select():
+    return render_template('randomize/randomize.html')
+
+@app.route('/upload_file')
+def upload_file():
+    return render_template('upload/upload.html')
+if __name__ == '__main__':
+    app.run()
