4c372f45e5e42d645969aaff979312b3c876b6f0
Hasan <hasanozdemir1@trakya.edu.tr>
2021-11-27 18:51:25 +0300

    move file method added to move converted file under static folder

---
+++ image_operations.py
@@ -0,0 +1,37 @@
+from os import path
+from pathlib import Path
+from PIL import Image
+from shutil import move
+
+
+def convert_to_webp(source):
+    """
+    Convert jpg, png image to WebP format.
+    :param: source <(pathlib.Path)> <str> Path to source image
+    :return: <pathlib.Path>  path to new image
+    """
+    destination = source.with_suffix(".webp")
+    # Open image
+    image = Image.open(source)
+    # Convert image to webp
+    image.save(destination, format="webp")
+
+    return destination
+
+# source : any
+# target : D:\\my_works\\Not_Important\\INTERVIEW-TASKS\\APSS_POC\\static\\img\\img_upload
+def move_converted_file(source: str, target_folder: str):
+    # separate folder and file name
+    head, file_name = path.split(source)
+    target = f"{target_folder}/{file_name}"
+    move(source, target)
+    return file_name
+
+
+if __name__ == '__main__':
+    #D:\\my_works\\Not_Important\\INTERVIEW-TASKS\\APSS_POC\\static\\img\\img_upload\\paşam1.jpg
+    # D:\\my_works\\Not_Important\\INTERVIEW-TASKS\\APSS_POC\\static\\img\\img_upload\\paşam2.jpg
+    #webp_path = str(convert_to_webp(Path("D:\\ART\\20201024_131103.jpg")))
+    #print(webp_path, type(webp_path))
+    source="D:/ART/ART20201024_131103.jpg"
+    move_converted_file(source,"D:/ART/2018-LifeEnd")
